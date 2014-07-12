8f7f152ebc66c40fb64a924e301eb674c99eff50
43081j <43081j@server.local>
2014-07-12 13:28:37 +0100

    add md5 support

--- integrity.js
+++ integrity.js
@@ -2,12 +2,13 @@
 	'use strict';
 
 	if(typeof define === 'function' && define.amd) {
-		define(['exports', 'lib/crc32'], factory);
+		define(['exports', 'lib/crc32', 'lib/md5'], factory);
 	} else if(typeof exports === 'object') {
-		factory(exports, require('./lib/crc32'));
+		factory(exports, require('./lib/crc32'), require('./lib/md5'));
 	} else {
-		factory((root.integrity = {}), root.integrity_crc32);
+		factory((root.integrity = {}), root.integrity_crc32, root.integrity_md5);
 	}
-})(this, function(exports, crc32) {
+})(this, function(exports, crc32, md5) {
 	exports.crc32 = crc32.crc32;
+	exports.md5 = md5.md5;
 });
